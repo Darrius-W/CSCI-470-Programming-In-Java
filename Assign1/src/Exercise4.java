72c983c51c19643c15e843f24a6ef4adc3929f98
Darrius-W <you@example.com>
2023-01-31 16:58:14 -0600

    Created getSum method

--- Assign1/src/Exercise4.java
+++ Assign1/src/Exercise4.java
@@ -1,3 +1,4 @@
+
 /*
  * Write an application that inputs three integers from the user and displays the sum,
  * average, product, smallest and largest of the numbers. Use the techniques shown in
@@ -5,6 +6,8 @@
  * in an integer representation of the average. So, if the sum of the values is 7,
  * the average should be 2, not 2.3333....]
  */
+import java.util.Scanner;
+
 public class Exercise4 {
     public static void main(String[] args) {
         // Scanner input
@@ -25,11 +28,19 @@ public class Exercise4 {
         int num3 = input.nextInt();
 
         // Print sum
+        System.out.printf("\nThe sum is: %d", ex4.getSum(num1, num2, num3));
         // Print average
         // Print smallest
         // print largest
+
+        input.close();
     }
+
     // Sum method
+    public int getSum(int num1, int num2, int num3) {
+        int result = num1 + num2 + num3;
+        return result;
+    }
     // Average method
     // Smallest integer method
     // Largest integer method
