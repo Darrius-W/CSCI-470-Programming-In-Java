048e91f15dc3ccfbb9857c4b7510c2aba607a2ad
Darrius-W <you@example.com>
2023-01-31 14:41:57 -0600

    Implemented Schema

---
+++ Assign1/src/Exercise3.java
@@ -0,0 +1,31 @@
+
+/*
+ * Prompt:
+ * -----------------------------------------------------------------------------
+ * Write an application that asks the user to enter two integers,
+ * obtains them from the user and prints their sum, product,
+ * difference and quotient (division). Use the techniques shown in Addition.java
+ * (i.e. Scanner, nextInt and printf)
+ */
+import java.util.Scanner;
+
+public class Exercise3 {
+    // Main
+    public static void main(String[] args) throws Exception {
+        // Scanner input
+        // Exercise3 object
+
+        // Collect Integer 1
+        // Collect Integer 2
+
+        // Print Sum
+        // Print Product
+        // Print Difference
+        // Print Quotient
+    }
+
+    // Sum method
+    // Product method
+    // Difference method
+    // Quotient method
+}
