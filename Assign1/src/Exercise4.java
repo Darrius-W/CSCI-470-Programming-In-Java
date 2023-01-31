39a0756e2a2bfe148988046f824713a89edda4f4
Darrius-W <you@example.com>
2023-01-31 17:23:17 -0600

    Added getMax method

--- Assign1/src/Exercise4.java
+++ Assign1/src/Exercise4.java
@@ -37,6 +37,7 @@ public class Exercise4 {
         // Print smallest
         System.out.printf("\nThe smallest integer is: %d", ex4.getMin(num1, num2, num3));
         // print largest
+        System.out.printf("\nThe largest integer is: %d", ex4.getMax(num1, num2, num3));
 
         input.close();
     }
@@ -63,7 +64,7 @@ public class Exercise4 {
             else {
                 return num2;
             }
-        } else // if (num2 > num1)
+        } else // if (num2 >= num1)
         {
             if (num1 >= num3) {
                 return num3;
@@ -74,5 +75,22 @@ public class Exercise4 {
             }
         }
     }
+
     // Largest integer method
+    public int getMax(int num1, int num2, int num3) {
+        if (num1 >= num2) {
+            if (num1 >= num3) {
+                return num1;
+            } else {
+                return num3;
+            }
+        } else // if (num2 >= num1)
+        {
+            if (num2 >= num3) {
+                return num2;
+            } else {
+                return num3;
+            }
+        }
+    }
 }
