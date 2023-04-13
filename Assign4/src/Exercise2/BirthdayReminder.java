47ec3bf7c5f5406df24d945118d886191e3c5bb4
Darrius-W <you@example.com>
2023-04-13 15:49:45 -0500

    Added Assign 4 Exercise 3

--- Assign4/src/Exercise2/BirthdayReminder.java
+++ Assign4/src/Exercise2/BirthdayReminder.java
@@ -1,15 +1,3 @@
-
-/*
- * Write an application that allows a user to enter the names and birthdates
- * of up to 10 friends. Continue to prompt the user for names and birthdates
- * until the user enters the sentinel value ZZZ for a name or has entered 10 names,
- * whichever comes first. When the user is finished entering names, produce a
- * count of how many names were entered, and then display the names. In a loop,
- * continuously ask the user to type one of the names and display the corresponding
- * birthdate or an error message if the name has not been previously entered. The
- * loop continues until the user enters ZZZ for a name. Save the application as
- * BirthdayReminder.java.
- */
 import java.util.Scanner;
 import java.util.Arrays;
 
---
+++ Assign4/src/Exercise3/CarCareChoice2.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class CarCareChoice2 {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        String userOption;
+        String userOptionShort;
+        String[] shopOptions = { "oil change", "tire rotation", "battery check", "brakes" };
+        String[] shopOptionsShort = { "oil", "tir", "bat", "bra" };
+
+        // Display menu for Cody's Car Care Shop
+        System.out.println("Cody's Care Care Shop");
+        System.out.println("---------------------");
+        System.out.println("What can we do for you today?");
+        System.out.println("- Oil Change");
+        System.out.println("- Tire Rotation");
+        System.out.println("- Battery Check");
+        System.out.println("- Brakes");
+
+        // Request user string input
+        System.out.print("\nOption: ");
+        userOption = input.nextLine();
+        userOption = userOption.toLowerCase();
+
+        // Collect shortened userOption to account for spelling mistakes
+        userOptionShort = userOption.substring(0, 3);
+
+        // Switch statement to check options against user input
+        switch (userOptionShort) {
+            // Option: Oil change
+            case "oil":
+                // Display price $25
+                System.out.printf("%s is $25%n", shopOptions[0]);
+                break;
+
+            // Option: Tire rotation
+            case "tir":
+                // Display price $22
+                System.out.printf("%s is $22%n", shopOptions[1]);
+                break;
+
+            // Option: Battery check
+            case "bat":
+                // Display price $15
+                System.out.printf("%s is $15%n", shopOptions[2]);
+                break;
+
+            // Option: Brakes
+            case "bra":
+                // Display price $5
+                System.out.printf("%s are $5%n", shopOptions[3]);
+                break;
+
+            // Default Option: Error
+            default:
+                // Display Error Output
+                System.out.println("***ERROR: INVALID OPTION***");
+                break;
+        }
+
+        input.close();
+    }
+}
